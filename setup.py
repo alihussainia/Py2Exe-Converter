7cf060a3c287a9709542becef6f055a3307d328b
MUHAMMAD ALI <malirashid1994@gmail.com>
2021-08-08 08:43:21 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     long_description = """The package allows users (linux) to convert python scripts into executable files by simply using the py2exe-converter -f <python-file-name.py>. 
     One can also use another option after entering python filename which is -p for packages to be included into the executable file. -p takes requirement.txt as input. I have included an example in the demo folder as well. Enjoy! And yup, please, do come forward if you think you can improve or add more functionalities to the project :). 
     Lastly, a huge shoutout to pyinstaller developers who made such an incredible solution that provided a solid base to this project. Thanks amigos!""",
-    name = 'py2exe-converter',
-    packages = ['py2exe-converter'],
+    name = 'py2exe_converter',
+    packages = ['py2exe_converter'],
     version = 'v1.0.0',
     description = 'Python to Exe file converter package',
     author = 'Muhammad Ali',
